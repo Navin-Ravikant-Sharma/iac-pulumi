4470130e5878d0df6adbb2af6ff4b74d4c42cf3f
Navin <sharma.navi@northeastern.edu>
2023-10-11 16:50:56 -0400

    get availability count

--- index.js
+++ index.js
@@ -21,24 +21,30 @@ const available = aws.getAvailabilityZones({
 });
 
 for (let i = 0; i < 3; i++) {
-    const subnet = new aws.ec2.Subnet(`publicSubnet${i}`, {
+    const pubsubnet = new aws.ec2.Subnet(`publicSubnet${i}`, {
         vpcId: myvpc.id,
         availabilityZone: available.then(available => available.names?.[i]),
         cidrBlock: pulumi.interpolate`10.0.${i}.0/24`,
         mapPublicIpOnLaunch: true,
+        tags: {
+            Name: "public subnet",
+        },
     });
-    publicSubnets.push(subnet);
+    publicSubnets.push(pubsubnet);
 }
 
 // Create private subnets
 const privateSubnets = [];
 for (let i = 0; i < 3; i++) {
-    const subnet = new aws.ec2.Subnet(`privateSubnet${i}`, {
+    const privsubnet = new aws.ec2.Subnet(`privateSubnet${i}`, {
         vpcId: myvpc.id,
         availabilityZone: available.then(available => available.names?.[i]),
         cidrBlock: pulumi.interpolate`10.0.${i + 10}.0/24`,
+        tags: {
+            Name: "private subnet",
+        },
     });
-    privateSubnets.push(subnet);
+    privateSubnets.push(privsubnet);
 }
 
 //creating internet gateway
@@ -53,6 +59,9 @@ const internet = new aws.ec2.InternetGateway("internetGateway", {
 // Create a public route table
 const pubRouteTable = new aws.ec2.RouteTable("pubRouteTable", {
     vpcId: myvpc.id,
+    tags: {
+        Name: "public route table",
+    },
 });
 
 // Attach all public subnets to the public route table
@@ -66,6 +75,9 @@ publicSubnets.forEach((subnet, index) => {
 // Create a private route table
 const privRouteTable = new aws.ec2.RouteTable("privRouteTable", {
     vpcId: myvpc.id,
+    tags: {
+        Name: "private route table",
+    },
 });
 
 // Attach all private subnets to the private route table
@@ -80,4 +92,7 @@ const publicRoute = new aws.ec2.Route("pubRoute", {
     routeTableId: pubRouteTable.id,
     destinationCidrBlock: process.env.destination_cidr,
     gatewayId: internet.id,
+    tags: {
+        Name: "public route for destination",
+    },
 });
